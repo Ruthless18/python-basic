f3e536f371aa9fce7c2a2634bab9c12279420d62
Vladislav Glagolev <khfdh@yandex.ru>
2022-09-15 22:54:22 +0300

    create return for function power_numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,14 +4,14 @@
 """
 
 
-def power_numbers():
+def power_numbers(*nums):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-
+    return [num ** 2 for num in nums]
 
 # filter types
 ODD = "odd"
