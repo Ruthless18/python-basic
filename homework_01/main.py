ceca78b20f5ce1fc089ae0b4f51d39354709963a
Vladislav Glagolev <khfdh@yandex.ru>
2022-09-22 17:43:06 +0300

    edit is_prime

--- homework_01/main.py
+++ homework_01/main.py
@@ -20,13 +20,14 @@ PRIME = "prime"
 
 
 def is_prime(nums):
-    if nums == 1 or 0:
-        return False
-    for num in range(2, int(nums ** 0.5 + 1) ):
-        if nums % num == 0:
+    for num in nums:
+        if num < 2:
             return False
-    else:
-        return True
+        for num in range(2, int(num ** 0.5 + 1) ):
+            if nums % num == 0:
+                return False
+        else:
+            return True
 
 
 def filter_numbers(nums, filter_type):
